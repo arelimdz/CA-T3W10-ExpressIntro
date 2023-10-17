ab7f2cacce20f7d2565cc98ad38ee4ca97c9ceb2
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-17 20:52:23 +1100

    feat: fetch 3er party api with asyn

---
+++ src/controllers/PokemonController.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+// create an instance of the Express router 
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+	response.json({
+		message:"Hello world from a router!"
+	});
+});
+
+// Create out of CRUD 
+router.post("/", (request, response) => {
+	response.json({
+		message:"POST request received!"
+	})
+});
+
+
+
+// router.get("/bananas", (request, response) => {
+
+// 	response.json({
+// 		message: "bananas!"
+// 	});
+// });
+
+
+router.get("/bananas", async (request, response) => {
+
+	let result = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+	let data = await result.json();
+
+
+	response.json({
+		message: "bananas!",
+		pokemonName: data.name
+	});
+});
+
+
+module.exports = router;
+
+// module.exports = {
+// 	PokemonRouter: router
+// }
+
+
